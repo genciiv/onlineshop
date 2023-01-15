443ffb172bd4387058a86362e346b5a5cbaee4fc
G-code <vaqogenci@gmail.com>
2023-01-15 20:15:26 +0100

    created categories

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -107,6 +107,81 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      <section className='home-wrapper-2 py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='categories d-flex justify-content-between align-items-center flex-wrap'>
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Muzik & Lojera</h6>
+                    <p>10 Cope</p>
+                  </div>
+                  <img src='images/camera.jpg' alt='camera' />
+                </div>
+
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Kamera</h6>
+                    <p>10 Cope</p>
+                  </div>
+                  <img src='images/camera.jpg' alt='camera' />
+                </div>
+
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Smart Tv</h6>
+                    <p>10 Cope</p>
+                  </div>
+                  <img src='images/tv.jpg' alt='camera' />
+                </div>
+
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Smart Watches</h6>
+                    <p>10 Cope</p>
+                  </div>
+                  <img src='images/headphone.jpg' alt='camera' />
+                </div>
+
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Laptop</h6>
+                    <p>10 Cope</p>
+                  </div>
+                  <img src='images/laptop.jpg' alt='camera' />
+                </div>
+
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Celulare</h6>
+                    <p>10 Cope</p>
+                  </div>
+                  <img src='images/camera.jpg' alt='camera' />
+                </div>
+
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Smart Tv</h6>
+                    <p>10 Cope</p>
+                  </div>
+                  <img src='images/tv.jpg' alt='camera' />
+                </div>
+
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Smart Watches</h6>
+                    <p>10 Cope</p>
+                  </div>
+                  <img src='images/headphone.jpg' alt='camera' />
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
